7b7a86f80372a8365cdd9e2fda9add271d671158
Sören Meier <soeren@s-me.ch>
2021-09-01 02:44:05 +0200

    [service-bootloader-api] add AsyncClient

--- service-bootloader-api/src/lib.rs
+++ service-bootloader-api/src/lib.rs
@@ -1,6 +1,7 @@
 
 use std::io;
 use std::borrow::Cow;
+use std::process::Child;
 
 use stdio_api::{Stdio, Line, Kind};
 
@@ -10,16 +11,53 @@ pub struct Server {
 
 impl Server {
 
+	/// Returns none if the child doesn't have stdin and stdout
+	pub fn new(child: &mut Child) -> Option<Self> {
+		Stdio::from_child(child)
+			.map(|inner| Self { inner })
+	}
+
 	pub fn receive(&mut self) -> io::Result<Option<Request>> {
 		let r = self.inner.read()?
-			.map(Request::from_line);
+			.map(Request::from);
 		Ok(r)
 	}
 
 }
 
 
+#[cfg(any(feature = "async", test))]
+mod r#async {
+
+	use super::*;
+	use stdio_api::AsyncStdio;
+
+	pub struct AsyncClient {
+		inner: AsyncStdio
+	}
+
+	impl AsyncClient {
+
+		pub fn new() -> Self {
+			Self {
+				inner: AsyncStdio::from_env()
+			}
+		}
+
+		pub async fn send(&mut self, req: Request) -> io::Result<()> {
+			self.inner.write(&req.into()).await
+		}
+
+	}
+
+}
+#[cfg(any(feature = "async", test))]
+pub use r#async::*;
+
+
+
 // if you receive a message it will be this
+#[derive(Debug, Clone)]
 pub enum Request {
 	SystemdRestart { name: String },
 	// this can even be a response
@@ -27,6 +65,7 @@ pub enum Request {
 }
 
 impl Request {
+
 	pub fn key(&self) -> &str {
 		match self {
 			Self::SystemdRestart {..} => "SystemdRestart",
@@ -41,7 +80,16 @@ impl Request {
 		}
 	}
 
-	pub fn from_line(line: Line) -> Self {
+}
+
+impl From<Request> for Line {
+	fn from(req: Request) -> Line {
+		Line::new(Kind::Request, req.key(), req.data().as_ref())
+	}
+}
+
+impl From<Line> for Request {
+	fn from(line: Line) -> Self {
 		if !matches!(line.kind(), Kind::Request) {
 			return Self::Unknown(line)
 		}
@@ -52,11 +100,5 @@ impl Request {
 			},
 			_ => Self::Unknown(line)
 		}
-	}
-}
-
-impl From<Request> for Line {
-	fn from(req: Request) -> Line {
-		Line::new(Kind::Request, req.key(), req.data().as_ref())
 	}
 }
