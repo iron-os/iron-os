938a8aebb1fb8bf52e4cf1c0967f7b5aae1be8d1
Sören Meier <soeren@s-me.ch>
2021-09-04 02:37:49 +0200

    [stdio-api] add serialize, deserialize

--- stdio-api/src/lib.rs
+++ stdio-api/src/lib.rs
@@ -69,6 +69,52 @@ impl Line {
 //:>:Request
 //:<:Response
 
+#[cfg(feature = "serde")]
+mod serde {
+
+	use std::fmt;
+
+	use _serde::{Serialize, Deserialize};
+
+	#[derive(Debug)]
+	pub enum Error {
+		Json(serde_json::Error),
+		ContainsInvalidChars
+	}
+
+	impl fmt::Display for Error {
+		fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+			match self {
+				Self::Json(e) => e.fmt(f),
+				Self::ContainsInvalidChars => f.write_str("ContainsInvalidChars")
+			}
+		}
+	}
+
+	impl std::error::Error for Error {}
+
+	// not allowed to contain \n
+	pub fn serialize<T>(value: &T) -> Result<String, Error>
+	where T: Serialize + ?Sized {
+		let s = serde_json::to_string(value)
+			.map_err(Error::Json)?;
+		s.contains('\n')
+			.then(|| s)
+			.ok_or(Error::ContainsInvalidChars)
+	}
+
+	pub fn deserialize<'a, T>(s: &'a str) -> Result<T, Error>
+	where T: Deserialize<'a> {
+		serde_json::from_str(s)
+			.map_err(Error::Json)
+	}
+
+}
+#[cfg(feature = "serde")]
+pub use serde::{serialize, deserialize};
+pub use serde::Error as SerdeError;
+
+
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Kind {
