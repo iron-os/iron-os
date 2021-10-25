51e13ccd1b6366c1b81b52a86f6784701943b456
Sören Meier <soeren@s-me.ch>
2021-10-25 16:04:37 +0200

    [service] cleanup

--- service/pub_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cargo build --release
-rsync ./target/release/service user@192.168.14.62:/data/packages/service/left/
-rsync -r ./extension user@192.168.14.62:/data/packages/service/left/
-rsync -r ./www user@192.168.14.62:/data/packages/service/left/
--- service/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf ./www/fire-html
-cp -r ../../fire-html/src ./www/fire-html
-rm -rf ./extension/fire-html
-cp -r ../../fire-html/src ./extension/fire-html
