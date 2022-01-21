2183e4864ce7d3ef2c46c9063cfefb597c9394b2
Sören Meier <soeren@s-me.ch>
2022-01-21 17:57:32 +0100

    [example-binary] add --disks and --install flags

--- example-binary/src/main.rs
+++ example-binary/src/main.rs
@@ -1,16 +1,55 @@
 
+mod service_api;
+
 use tokio::time::{sleep, Duration};
 
-mod service_api;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+struct Args {
+	/// List all disks that could be used to install on
+	#[clap(long = "disks")]
+	list_disks: bool,
+	/// Install on a specific disk
+	#[clap(long)]
+	install: Option<String>
+}
 
 #[tokio::main]
 async fn main() {
+	let args = Args::parse();
 
 	let client = service_api::Client::connect().await
 		.expect("could not connect to the service api");
 
+	if args.list_disks {
+		let disks = client.list_disks().await
+			.expect("failed to list disks");
+
+		println!("{:>8} | {:>4} | {:>8}", "Name", "Init", "Size");
+		for disk in disks {
+			println!(
+				"{:>8} | {:>4} | {:>8.0}gb",
+				disk.name,
+				disk.initialized,
+				disk.size / 1000_000_000
+			);
+		}
+
+		return;
+	}
+
+	if let Some(disk) = args.install {
+		println!("Installing... to {}", disk);
+		client.install_on(disk).await
+			.expect("failed to install");
+		println!("Installation completed");
+		return;
+	}
+
 	// wait until a connection could be established
-	sleep(Duration::from_secs(30).await;
+	sleep(Duration::from_secs(30)).await;
 
 	// let's open youtube
 	client.open_page("https://youtube.com").await
--- example-binary/src/service_api.rs
+++ example-binary/src/service_api.rs
@@ -5,6 +5,8 @@ use service_api::error::Result;
 use service_api::client::Client as ApiClient;
 
 use service_api::requests::ui::OpenPageReq;
+use service_api::requests::device::{DisksReq, Disk};
+use service_api::requests::system::InstallOnReq;
 
 /// This does not reconnect, since if the connection closes we expect
 /// to be restarted
@@ -23,6 +25,16 @@ impl Client {
 
 	pub async fn open_page(&self, url: impl Into<String>) -> Result<()> {
 		self.inner.request(OpenPageReq { url: url.into() }).await
+			.map(|_| ())
+	}
+
+	pub async fn list_disks(&self) -> Result<Vec<Disk>> {
+		self.inner.request(DisksReq).await
+			.map(|d| d.0)
+	}
+
+	pub async fn install_on(&self, disk: String) -> Result<()> {
+		self.inner.request(InstallOnReq { name: disk }).await
 			.map(|_| ())
 	}
 }
