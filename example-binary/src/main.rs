767f4ee2daea785ba377d4c1ef08e0e54cde668d
Sören Meier <soeren@s-me.ch>
2023-05-18 18:50:22 +0200

    [example-binary] v1.1.0 add connect-wifi command

--- example-binary/src/main.rs
+++ example-binary/src/main.rs
@@ -3,6 +3,7 @@ use tokio::time::{sleep, Duration};
 use clap::Parser;
 
 use service_api::client::Client;
+use service_api::requests::network::{AddConnectionKind, AddConnectionWifi};
 
 
 #[derive(Debug, Parser)]
@@ -28,7 +29,10 @@ enum SubCommand {
 	/// List Access Points
 	ListAccessPoints(ListAccessPoints),
 	/// List Connections
-	ListConnections(ListConnections)
+	ListConnections(ListConnections),
+	/// Connecte to Wifi
+	ConnectWifi(ConnectWifi)
+
 }
 
 #[derive(Debug, Parser)]
@@ -57,6 +61,16 @@ struct ListAccessPoints {}
 #[derive(Debug, Parser)]
 struct ListConnections {}
 
+/// Connect to a wifi which was listed in list access point
+///
+/// should be a wpa-psk access point
+#[derive(Debug, Parser)]
+struct ConnectWifi {
+	pub interface_name: String,
+	pub ssid: String,
+	pub password: String
+}
+
 #[tokio::main]
 async fn main() {
 	let args = Args::parse();
@@ -78,67 +92,71 @@ async fn main() {
 					disk.size / 1000_000_000
 				);
 			}
-
-			return;
 		},
 		Some(SubCommand::Install(i)) => {
 			println!("Installing... to {}", i.disk);
 			client.install_on(i.disk).await
 				.expect("failed to install");
 			println!("Installation completed");
-			return
 		},
 		Some(SubCommand::DeviceInfo(_)) => {
 			println!("Display device info");
 			let device_info = client.device_info().await
 				.expect("failed to get device info");
 			println!("{:#?}", device_info);
-			return
 		},
 		Some(SubCommand::SystemInfo(_)) => {
 			println!("Display system info");
 			let system_info = client.system_info().await
 				.expect("failed to get system info");
 			println!("{:#?}", system_info);
-			return
 		},
 		Some(SubCommand::ListPackages(_)) => {
 			println!("Display packages");
 			let packages = client.list_packages().await
 				.expect("failed to list packages");
 			println!("{:#?}", packages);
-			return
 		},
 		Some(SubCommand::UpdateNow(_)) => {
 			println!("update now");
 			client.request_update().await
 				.expect("failed to request update");
 			println!("update done");
-			return
 		},
 		Some(SubCommand::ListAccessPoints(_)) => {
 			println!("access points");
 			let list = client.network_access_points().await.unwrap();
 			println!("{:#?}", list);
-			return
 		},
 		Some(SubCommand::ListConnections(_)) => {
 			println!("connections");
 			let list = client.network_connections().await.unwrap();
 			println!("{:#?}", list);
-			return
+		},
+		Some(SubCommand::ConnectWifi(conn)) => {
+			println!("connect to wifi {}", conn.ssid);
+			let conn = client.network_add_connection(
+				format!("wifi-{}", conn.ssid),
+				AddConnectionKind::Wifi(AddConnectionWifi {
+					interface_name: conn.interface_name,
+					ssid: conn.ssid,
+					password: conn.password
+				})
+			).await.unwrap();
+			println!("connected to {:?}", conn);
+		},
+		None => {
+			println!("opening https://youtube.com in 30s");
+			// wait until a network connection could be established
+			sleep(Duration::from_secs(30)).await;
+
+			// let's open youtube
+			client.open_page("https://youtube.com".into()).await
+				.expect("could not open youtube");
+
+			loop {
+				sleep(Duration::from_secs(5 * 60)).await
+			}
 		}
-		None => {}
-	}
-
-	// wait until a network connection could be established
-	sleep(Duration::from_secs(30)).await;
-
-	// let's open youtube
-	client.open_page("https://youtube.com".into()).await
-		.expect("could not open youtube");
-
-	loop {
-		sleep(Duration::from_secs(5 * 60)).await
 	}
 }
